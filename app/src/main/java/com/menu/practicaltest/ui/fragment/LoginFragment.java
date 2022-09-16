ab24f8d78219c26f9813d3dc8cb1ecfa422ef7cd
YoungYoda10 <vladimir.vozar2000@gmail.com>
2022-09-17 01:35:41 +0200

    Added LoginFragmentViewModel

--- app/src/main/java/com/menu/practicaltest/ui/fragment/LoginFragment.java
+++ app/src/main/java/com/menu/practicaltest/ui/fragment/LoginFragment.java
@@ -10,12 +10,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.gson.Gson;
 import com.menu.practicaltest.databinding.FragmentLoginBinding;
 import com.menu.practicaltest.repository.retrofit.RetrofitClient;
 import com.menu.practicaltest.repository.retrofit.body.UserData;
 import com.menu.practicaltest.repository.retrofit.response.LoginResponse;
+import com.menu.practicaltest.ui.viewmodel.LoginFragmentViewModel;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,6 +26,7 @@ import retrofit2.Response;
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
+    private LoginFragmentViewModel viewModel;
 
     public LoginFragment() {
     }
@@ -32,6 +35,8 @@ public class LoginFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this).get(LoginFragmentViewModel.class);
+
         return binding.getRoot();
     }
 
@@ -44,41 +49,10 @@ public class LoginFragment extends Fragment {
             String password = binding.etPassword.getText().toString().trim();
 
             if (!email.isEmpty() && !password.isEmpty()) {
-//                loginUser(new UserData(email, password));
-                loginUser(new UserData("test@testmenu.app", "test1234"));
+                viewModel.loginUser(new UserData(email, password));
             } else {
                 Toast.makeText(getContext(),
                         "Email or Password is Empty",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    private void loginUser(UserData userData) {
-        Call<LoginResponse> call = RetrofitClient.getInstance().getMyApi().loginUser(userData);
-        call.enqueue(new Callback<LoginResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<LoginResponse> call,
-                                   @NonNull Response<LoginResponse> response) {
-                if (response.isSuccessful()) {
-                    LoginResponse loginResponse = response.body();
-                    Toast.makeText(getContext(),
-                            "Logged In",
-                            Toast.LENGTH_LONG).show();
-                    Log.i("loginResponse", new Gson().toJson(loginResponse));
-                } else {
-                    Toast.makeText(getContext(),
-                            "An error has occurred",
-                            Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<LoginResponse> call,
-                                  @NonNull Throwable t) {
-                Toast.makeText(getContext(),
-                        "An error has occurred",
                         Toast.LENGTH_LONG).show();
             }
         });
---
+++ app/src/main/java/com/menu/practicaltest/ui/viewmodel/LoginFragmentViewModel.java
@@ -0,0 +1,56 @@
+package com.menu.practicaltest.ui.viewmodel;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.google.gson.Gson;
+import com.menu.practicaltest.repository.retrofit.RetrofitClient;
+import com.menu.practicaltest.repository.retrofit.body.UserData;
+import com.menu.practicaltest.repository.retrofit.response.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LoginFragmentViewModel extends AndroidViewModel {
+
+    private final RetrofitClient retrofitClient = RetrofitClient.getInstance();
+
+    public LoginFragmentViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    public void loginUser(UserData userData) {
+        Call<LoginResponse> call = retrofitClient.getMyApi().loginUser(userData);
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<LoginResponse> call,
+                                   @NonNull Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    LoginResponse loginResponse = response.body();
+                    Toast.makeText(getApplication(),
+                            "Logged In",
+                            Toast.LENGTH_LONG).show();
+                    Log.i("loginResponse", new Gson().toJson(loginResponse));
+                } else {
+                    Toast.makeText(getApplication(),
+                            "An error has occurred",
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<LoginResponse> call,
+                                  @NonNull Throwable t) {
+                Toast.makeText(getApplication(),
+                        "An error has occurred",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
