a67705bcdc3b2278909780a4832ee067f142d279
YoungYoda10 <vladimir.vozar2000@gmail.com>
2022-09-17 00:18:42 +0200

    Created VenuesFragment and added recyclerView in layout

---
+++ app/src/main/java/com/menu/practicaltest/ui/fragment/VenuesFragment.java
@@ -0,0 +1,39 @@
+package com.menu.practicaltest.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.menu.practicaltest.R;
+import com.menu.practicaltest.databinding.FragmentVenuesBinding;
+
+public class VenuesFragment extends Fragment {
+
+    private FragmentVenuesBinding binding;
+
+    public VenuesFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentVenuesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
